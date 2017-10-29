b6c72bb4aa0399609d24f4d9d7df1ca04574526c
Miki Stanger <github@foge.us>
2017-10-29 22:51:58 +0200

    Default comparison now performs deep comparison on objects.
    When a configuration's value is set to true, react-managed-update-component will use the new isDifferentWithDeepComparison instead of a basic !==.
    This function:
     - Checks if both given values are objects.
     - If not, it returns their strict-unequality.
     - If they are, deep comparison is made, using isDifferentWithDeepComparison recursively.
    
     isDifferentWithDeepComparison offers a few optimizations. Before going recursive it:
     - Checks if both values are equal
     - Checks if there's a difference in both values keys, by comparing the number of keys of each one, checking if theres a different key name. This is done by sorting and then iterating.

--- src/index.js
+++ src/index.js
@@ -20,11 +20,35 @@ class ManagedUpdateComponent extends React.Component {
   shouldUpdateByObject(currentObj, nextObj, testsObject) {
     return Object.keys(testsObject).every(key => {
       if (testsObject[key] === false) { return true }
-      if (testsObject[key] === true) { return currentObj[key] !== nextObj[key] }
+      if (testsObject[key] === true) { return isDifferentWithDeepComparison(currentObj, nextObj[key]) }
       if (testsObject[key].constructor.name === 'Function') { return testsObject[key](currentObj[key], nextObj[key]) }
       throw new Error('The value of a getShouldUpdateRelevantKeys object should be either boolean or function.')
     })
   }
+}
+
+function isDifferentWithDeepComparison(a, b) {
+  if (a instanceof Object && b instanceof Object) {
+    if (a === b) {
+      return false
+    }
+    const aKeys = Object.keys(a)
+    const bKeys = Object.keys(b)
+    if (aKeys.length === bKeys.length) {
+      const sortedAKeys = aKeys.sort()
+      const sortedBKeys = bKeys.sort()
+      if (sortedAKeys.some((key, index) => key !== sortedBKeys[index])) {
+        return true
+      }
+    }
+    const uniqueKeysObject = aKeys.concat(bKeys).reduce((agg, key) => {
+      agg[key] = true
+      return agg
+    }, {})
+    const keys = Object.keys(uniqueKeysObject)
+    return keys.some(key => isDifferentWithDeepComparison(a[key], b[key]))
+  }
+  return a !== b
 }
 
 export default ManagedUpdateComponent
